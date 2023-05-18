af1dab37bd0a14e6c8747b7fbdac3f6d1c5d9912
Egor Kisel <kisel1989@mail.ru>
2023-05-18 03:34:41 +0300

    Added BottomSheetBehavior with settings

--- app/src/main/java/com/example/phoneshop/view/FragmentMain.kt
+++ app/src/main/java/com/example/phoneshop/view/FragmentMain.kt
@@ -1,14 +1,17 @@
 package com.example.phoneshop.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.phoneshop.KEY_BUNDLE
 import com.example.phoneshop.R
 import com.example.phoneshop.databinding.FragmentMainBinding
@@ -16,6 +19,7 @@ import com.example.phoneshop.view.adapter.BestSellerAdapter
 import com.example.phoneshop.view.adapter.HomeStoreAdapter
 import com.example.phoneshop.viewmodel.AppState
 import com.example.phoneshop.viewmodel.StoreViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class FragmentMain : Fragment(), OnItemClickListener {
 
@@ -26,6 +30,7 @@ class FragmentMain : Fragment(), OnItemClickListener {
     }
     private val adapterHome = BestSellerAdapter()
     private val adapterBest = HomeStoreAdapter()
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,13 +47,38 @@ class FragmentMain : Fragment(), OnItemClickListener {
         binding.recyclerHomeStorePhones.adapter = adapterHome
         adapterHome.mSetOnClickListener(this)
         binding.recyclerBestSeller.adapter = adapterBest
-        binding.recyclerBestSeller.layoutManager =
-            LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
         viewModel.apply {
             getLiveData().observe(viewLifecycleOwner, observer)
             getHomeStore()
             getBestSeller()
         }
+        setHasOptionsMenu(true)
+
+        setBottomSheetBehavior()
+
+        binding.behaviorInclude.btnClose.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
+    }
+
+    private fun setBottomSheetBehavior() {
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.behaviorInclude.bottomSheetContainer)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.filter -> {
+                Log.d("@@@", "Filter")
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun renderData(state: AppState) {
